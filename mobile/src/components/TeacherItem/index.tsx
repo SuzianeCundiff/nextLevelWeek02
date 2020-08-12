d50f43d2b1c6604b1df9ab719db1b777d9ec1bb1
Suziane Cundiff <suzianecundiff@gmail.com>
2020-08-12 16:09:03 -0400

    Update 10
    Implementado o Front-End das páginas 'Favorites' e 'TeacherList', com a adição do componente 'TeacherItem', e do mecanismo de filtragem de Aulas (em 'TeacherList').

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+
+import styles from './styles';
+
+function TeacherItem() {
+    return(
+        <View style={styles.container}>
+            <View style={styles.profile}>
+                <Image 
+                    style={styles.avatar}
+                    source={{ uri: 'https://github.com/SuzianeCundiff.png'}}
+                />
+
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>Bear Nando</Text>
+                    <Text style={styles.subject}>Química</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                {'\n'}{'\n'}
+                Officia, inventore quam? Dolorem, dicta cupiditate illum iste, assumenda inventore in id quidem nemo, vero voluptatibus quia minima illo eius? Culpa, aut?
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora {' '}
+                    <Text style={styles.priceValue}>R$ 300,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
+                        {/* <Image source={heartOutlineIcon}/> */}
+                        <Image source={unfavoriteIcon}/>
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon}/>
+                        <Text style={styles.contactButtonText}>Entrar em Contato</Text>
+                    </RectButton>
+                </View>
+            </View>
+
+        </View>
+    );
+}
+
+export default TeacherItem;
